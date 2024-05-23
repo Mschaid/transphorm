9b24681c410d885752e7d4d18c32acd4a5ca6fdd
mschaid <michael.schaid@northwestern.edu>
2024-05-23 16:22:55 -0700

    full commit after protyping

---
+++ scratch/aws_connect_test.py
@@ -0,0 +1,9 @@
+# import boto3
+
+# s3 = boto3.client("s3")
+
+# bucket_name = "full-timeseries"
+# repsonse = s3.list_objects_v2(Bucket=bucket_name)
+
+# for i in repsonse["Contents"]:
+#     print(i)
