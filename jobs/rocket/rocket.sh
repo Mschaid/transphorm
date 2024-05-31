20516bc6206776d01a25977499430ee52be375b0
mschaid <michael.schaid@northwestern.edu>
2024-05-31 15:52:48 -0700

    rocket job

--- jobs/rocket/rocket.sh
+++ jobs/rocket/rocket.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-SLACK_WEBHOOK = 'https://hooks.slack.com/triggers/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'
+# SLACK_WEBHOOK = 'https://hooks.slack.com/triggers/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'
 
-JOB_NAME = 'rocket_trainer'
+# JOB_NAME = 'rocket_trainer'
 #SBATCH --account=p31961
 
 #SBATCH --partition=normal
 
-#SBATCH --nodes=1
+#SBATCH --nodes=10
 
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=40
 
-#SBATCH --mem=1G
+#SBATCH --mem=10G
 
-#SBATCH --time=1:00:00
+#SBATCH --time=12:00:00
 
-#SBATCH --job-name=$JOB_NAME
+#SBATCH --job-name='rocket_trainer'
 
 #SBATCH --output=rocket_trainer.out
 # curl -X POST\ $SLACK_WEBHOOK \
