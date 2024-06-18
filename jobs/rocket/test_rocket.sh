1868b10fe77edda000b6cf44d5d7e3046ddec013
Mschaid <michael.schaid@northwestern.edu>
2024-06-18 15:52:36 -0500

    rocket job

--- jobs/rocket/test_rocket.sh
+++ jobs/rocket/test_rocket.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# SLACK_WEBHOOK = 'https://hooks.slack.com/triggers/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'
 
-#JOB_NAME = 'rocket_trainer'
 #SBATCH --account=p31961
 
 #SBATCH --partition=normal
@@ -17,8 +15,9 @@
 
 #SBATCH --job-name='rocket_trainer'
 
-#curl -X POST -H 'Content-type: application/json' --data '{"text":"$JOB_NAME started"}' $SLACK_WEBHOOK 
+JOB_NAME='rocket_trainer'
 
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'${JOB_NAME}' started"}' $SLACK_WEBHOOK
 python '/projects/p31961/transphorm/transphorm/experiments/rocket/fake_rocket_test.py' 
 
-#curl -X POST -H 'Content-type: application/json' --data '{"text":"$JOB_NAME complete"}' $SLACK_WEBHOOK 
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'${JOB_NAME}' complete"}' $SLACK_WEBHOOK
