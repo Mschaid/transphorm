dd7fa489f6d27223789b28bb117d672cd3004ab2
Mschaid <michael.schaid@northwestern.edu>
2024-06-19 16:25:54 -0500

    rocket bayes log fix

--- transphorm/experiments/rocket/rocket_bayes_tuning.py
+++ transphorm/experiments/rocket/rocket_bayes_tuning.py
@@ -83,9 +83,7 @@ def main():
 
     """ load data"""
 
-    log.info(f"Loaded data with shape {X.shape, y.shape}")
     X, y = load_data()
-
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=42
     )
