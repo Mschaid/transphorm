0008e1deaea2e01fbee55b68c71ed65dad00bc6e
Mschaid <michael.schaid@northwestern.edu>
2024-06-19 12:54:10 -0500

    update rocket kernels to 30K

--- transphorm/experiments/rocket/rocket.py
+++ transphorm/experiments/rocket/rocket.py
@@ -34,7 +34,7 @@ def main():
     )
 
     rocket = RocketClassifier(
-        num_kernels=100000,
+        num_kernels=30000,
         n_features_per_kernel=10,
         rocket_transform="minirocket",
         n_jobs=-1,
