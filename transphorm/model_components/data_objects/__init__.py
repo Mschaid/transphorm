975f95cc8f02a180e39c44d0deacecab03c81972
mschaid <michael.schaid@northwestern.edu>
2024-06-24 15:38:42 -0700

    synthetic fp data w/ test passed

--- transphorm/model_components/data_objects/__init__.py
+++ transphorm/model_components/data_objects/__init__.py
@@ -1,3 +1,4 @@
 from transphorm.model_components.data_objects.aa_dataobjects import *
 from transphorm.model_components.data_objects.general_data_objects import *
 from transphorm.model_components.data_objects.synthetic_data_objects import *
+from transphorm.model_components.data_objects.synthetic_fp_data_objects import *
