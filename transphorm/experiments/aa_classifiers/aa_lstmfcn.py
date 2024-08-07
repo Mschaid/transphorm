044d034f67c087de64986fc3fa36311c119e43f4
Mschaid <michael.schaid@northwestern.edu>
2024-08-07 14:09:39 -0500

    lstmfcn exp

---
+++ transphorm/experiments/aa_classifiers/aa_lstmfcn.py
@@ -0,0 +1,30 @@
+
+
+from dotenv import load_dotenv
+from transphorm.model_components.data_objects import AATrialDataModule
+# from transphorm.framework_helpers.sk_helpers import dataloader_to_numpy
+import os
+
+def main():
+    # DATA_PATH = os.getenv('DATA_PATH')
+    # X, y =dataloader_to_numpy(DATA_PATH) 
+    print('test')
+#load data from torch 
+
+#spit data with reproduce
+
+
+#define lstmfcn model
+
+#train model
+
+#validate model
+
+# save model` `
+
+
+
+
+if __name__=='__main__':
+
+    main()
