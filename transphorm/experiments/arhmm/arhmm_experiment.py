b6698abb35d7d4f53e8449f4c37def1b0b00b644
mschaid <michael.schaid@northwestern.edu>
2024-09-03 12:24:46 -0700

    add ds

--- transphorm/experiments/arhmm/arhmm_experiment.py
+++ transphorm/experiments/arhmm/arhmm_experiment.py
@@ -121,7 +121,9 @@ def main():
     MODEL_SAVE_DIR.mkdir(parents=True, exist_ok=True)
     COMET_API_KEY = os.getenv("COMET_API_KEY")
     log.info("loading data")
-    x, labels = load_data(path=FULL_RECORDING_PATH, loader=AADataLoader)
+    x, labels = load_data(
+        path=FULL_RECORDING_PATH, loader=AADataLoader, down_sample=True
+    )
     log.info("configuring optimizer")
     opt = comet_ml.Optimizer(config=define_search_space())
     run_optimizer(
--- transphorm/preprocessors/loaders.py
+++ transphorm/preprocessors/loaders.py
@@ -59,7 +59,7 @@ class AADataLoader:
         """
         self._clean_data()
         if self.down_sample:
-            self.x = self.data[:, 1:][::100]
+            self.x = self.data[:, 1:][::1000]
         else:
             self.x = self.data[:, 1:]
 
