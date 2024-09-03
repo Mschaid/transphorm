6598c3741648de0dbe2c054dd1aba2ecefe6bd8e
mschaid <michael.schaid@northwestern.edu>
2024-09-03 13:23:53 -0700

    fix cmap

--- transphorm/analyzers/arhmm_analyzer.py
+++ transphorm/analyzers/arhmm_analyzer.py
@@ -1,3 +1,4 @@
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -8,9 +9,39 @@ from dotenv import load_dotenv
 from transphorm.framework_helpers import setup_comet_experimet
 from typing import List, Optional
 from torch import Tensor
-from dynamax.utils.plotting import gradient_cmap
+
 from transphorm.preprocessors.loaders import AADataLoader
 import polars as pl
+
+
+def gradient_cmap(colors, nsteps=256, bounds=None):
+    """Return a colormap that interpolates between a set of colors.
+    Ported from HIPS-LIB plotting functions [https://github.com/HIPS/hips-lib]
+    """
+    ncolors = len(colors)
+    # assert colors.shape[1] == 3
+    if bounds is None:
+        bounds = np.linspace(0, 1, ncolors)
+
+    reds = []
+    greens = []
+    blues = []
+    alphas = []
+    for b, c in zip(bounds, colors):
+        reds.append((b, c[0], c[0]))
+        greens.append((b, c[1], c[1]))
+        blues.append((b, c[2], c[2]))
+        alphas.append((b, c[3], c[3]) if len(c) == 4 else (b, 1.0, 1.0))
+
+    cdict = {
+        "red": tuple(reds),
+        "green": tuple(greens),
+        "blue": tuple(blues),
+        "alpha": tuple(alphas),
+    }
+
+    cmap = LinearSegmentedColormap("grad_colormap", cdict, nsteps)
+    return cmap
 
 
 class ARHMMAnalyzer:
